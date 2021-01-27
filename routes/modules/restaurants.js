78314196e97e47d735f08e33d6502f9a43fe349a
Flora Peng <berry1003@outlook.com>
2021-01-27 16:18:36 +0800

    refactor: modify express routes

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -8,29 +8,7 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const restaurantItem = req.body
-  let rest = {
-    name: restaurantItem.name,
-    category: restaurantItem.category,
-    image: restaurantItem.image,
-    location: restaurantItem.location,
-    phone: restaurantItem.phone,
-    google_map: restaurantItem.google_map,
-    rating: restaurantItem.rating,
-    description: restaurantItem.description
-  }
-
-  let { name, category, image, location, phone, google_map, rating, description } = rest
-
-  return Restaurant.create({
-    name,
-    category,
-    image,
-    location,
-    phone,
-    google_map,
-    rating,
-    description
-  })
+  return Restaurant.create(restaurantItem)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
@@ -54,29 +32,9 @@ router.get('/:id/edit', (req, res) => {
 router.put('/:id', (req, res) => {
   const id = req.params.id
   const restaurantItem = req.body
-  let rest = {
-    name: restaurantItem.name,
-    category: restaurantItem.category,
-    image: restaurantItem.image,
-    location: restaurantItem.location,
-    phone: restaurantItem.phone,
-    google_map: restaurantItem.google_map,
-    rating: restaurantItem.rating,
-    description: restaurantItem.description
-  }
-
-  let { name, category, image, location, phone, google_map, rating, description } = rest
-
   return Restaurant.findById(id)
     .then(restaurant => {
-      restaurant.name = name
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
+      restaurant = Object.assign(restaurant, restaurantItem)
       return restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${id}`))
